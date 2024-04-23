f09c7465582146960ad48de06517b175a9f87572
GurenSeiken <kevinsnivia@hotmail.com>
2024-04-23 05:50:37 -0500

    Boton de notis
    Botón de notificaciones, al presionar muestra las notificaciones

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
-import 'ui/songs_list.dart';
+import 'package:sound_frontend/src/ui/button_notis.dart';
+
 
 class App extends StatelessWidget {
     @override
     Widget build(BuildContext context) {
-        
         return MaterialApp(
 			theme: ThemeData.dark(),
-			home: Scaffold(
-				body: SongList(),
-			),
-        );
+      home: const Scaffold(
+        body: Center(
+          child:ButtonNotis(),
+        ),
+        ),
+      );
     }
 }
---
+++ lib/src/ui/button_notis.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:popover/popover.dart';
+import 'package:sound_frontend/src/ui/menu_items.dart';
+
+class ButtonNotis extends StatelessWidget{
+
+  const ButtonNotis({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return GestureDetector(
+      onTap: () => showPopover(
+      context: context,
+      bodyBuilder: (context)=> MenuItems(),
+      width: 250,
+      height: 180,
+      direction: PopoverDirection.bottom,
+      ),
+      child:const Icon(Icons.doorbell_outlined),
+    );
+  }
+
+
+}
